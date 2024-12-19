b62ce4a3a1b4491fd4edeab6323e92c48f41e617
Reza Madahi <reza.maddahi@outlook.com>
2024-12-19 22:51:19 +0330

    complete app

--- src/AnimalShow.js
+++ src/AnimalShow.js
@@ -1,7 +1,33 @@
+import './AnimalShow.css';
+import { useState } from "react";
+import bird from './svg/bird.svg';
+import cat from './svg/cat.svg';
+import cow from './svg/cow.svg';
+import dog from './svg/dog.svg';
+import gator from './svg/gator.svg';
+import heart from './svg/heart.svg';
+import horse from './svg/horse.svg';
 
-function AnimalShow() {
+const svgMap = {
+    bird,
+    cat,
+    cow,
+    dog,
+    gator,
+    horse
+};
+
+function AnimalShow(props) {
+    const [clicks, setClicks] = useState(0);
+    const handleClick = () => {
+        setClicks(clicks + 1);
+    };
+
     return(
-        <div>Cow!</div>
+        <div onClick={handleClick} className="animal-show">
+            <img alt={ props.type } src={ svgMap[props.type] } className="animal" />
+            <img alt="heart" src={ heart } style={ { width: 10 + 10 * clicks + 'px' } } className="heart" />
+        </div>
     );
 }
 
--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
-
+import './App.css';
 import { useState } from "react";
+import AnimalShow from "./AnimalShow";
+
+function getRandomAnimal() {
+    const animals = ['bird', 'cat', 'cow', 'dog', 'gator', 'horse'];
+    return animals[ Math.floor(Math.random() * animals.length) ];
+}
 
 function App() {
-
-    const [count, setCount] = useState(0);
+    const [animals, setAnimals] = useState([]);
 
     const handleClick = () => {
-        setCount(count + 1);
+        setAnimals([...animals, getRandomAnimal()]);
     };
 
+    const renderedAnimals = animals.map((animal, index) => {
+        return <AnimalShow type={animal} key={index} />
+    });
+
     return(
-        <div>
+        <div className="app">
             <button onClick={handleClick}>Add Animal</button>
-            <h4>Number of animals: {count}</h4>
+            <div className="animal-list">{ renderedAnimals }</div>
         </div>
     );
 }
